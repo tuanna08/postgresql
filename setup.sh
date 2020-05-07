4a3507b6a582c87194bb55adc9d70ff64472f24b
nguyen anh tuan <tuanna08go@gmail.com>
2020-05-07 14:02:03 +0700

    update script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Create by Openstack Company"
+
+#Install the repository RPM:
+yum install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+
+#Install the client packages:
+yum install postgresql12
+
+#Install the client packages:
+yum install postgresql12-server
+
+#Optionally initialize the database and enable automatic start
+/usr/pgsql-12/bin/postgresql-12-setup initdb
+systemctl enable postgresql-12
+systemctl start postgresql-12
+systemctl status postgresql-12
