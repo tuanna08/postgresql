c794e7efd64fe800e4937fe45b905ba40b1f29c2
Tuấn Nguyễn Anh <tuanna08go@gmail.com>
2020-07-16 13:44:32 +0700

    update

--- PostgreSQL Incremental Backup and Point-In-Time Recovery/Setting-Up-WAL-Archiving.sh
+++ PostgreSQL Incremental Backup and Point-In-Time Recovery/Setting-Up-WAL-Archiving.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 echo "==========Setting up WAL Archiving============"
 
-time_zone = "Asia/Ho_Chi_Minh"
-timedatectl set-timezone $time_zone && date
-echo $time_zone
+
+timedatectl set-timezone Asia/Ho_Chi_Minh && date
+
 mkdir /wal_archive
 chown -R postgres.postgres /wal_archive
 
 # read -s -p "Enter version postgresql (12): " my_version
 # echo "=============== $my_version"
 cp /etc/postgresql/12/main/postgresql.conf /etc/postgresql/12/main/postgresql.conf.bak
-echo "log_timezone: \'$time_zone\'"
-sed -i "s/^timezone = .*/timezone = '$time_zone'/g" /etc/postgresql/12/main/postgresql.conf
-sed -i "s/^log_timezone = .*/log_timezone = '$time_zone'/g" /etc/postgresql/12/main/postgresql.conf
+
+sed -i "s/^timezone = .*/timezone = 'Asia/Ho_Chi_Minh'/g" /etc/postgresql/12/main/postgresql.conf
+sed -i "s/^log_timezone = .*/log_timezone = 'Asia/Ho_Chi_Minh'/g" /etc/postgresql/12/main/postgresql.conf
 
 sed -i 's/^archive_mode =.*/archive_mode = on/g' /etc/postgresql/12/main/postgresql.conf
-sed -i "s/^archive_command =.*/archive_command = 'test \! \-f \/wal_archive/%f \&& cp \%p \/wal_archive/%f'/g" /etc/postgresql/12/main/postgresql.conf
+sed -i "s/^archive_command =.*/archive_command = \('test ! -f /wal_archive/%f && cp %p /wal_archive/%f'\)/g" /etc/postgresql/12/main/postgresql.conf
 sed -i 's/^wal_level =.*/wal_level = replica/g' /etc/postgresql/12/main/postgresql.conf
 
 systemctl restart postgresql
