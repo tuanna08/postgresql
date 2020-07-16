76a655af1b49367f76b0bcbbc9d3f3b25bd1689a
Tuấn Nguyễn Anh <tuanna08go@gmail.com>
2020-07-16 13:12:51 +0700

    update

--- PostgreSQL Incremental Backup and Point-In-Time Recovery/Setting-Up-WAL-Archiving.sh
+++ PostgreSQL Incremental Backup and Point-In-Time Recovery/Setting-Up-WAL-Archiving.sh
@@ -8,7 +8,7 @@ mkdir /wal_archive
 chown -R postgres.postgres /wal_archive
 
 read -s -p "Enter version postgresql (12): " my_version
-echo "$my_version"
+echo "=============== $my_version"
 
 sed -i "s/^timezone = .*/timezone = '$time_zone'/g" /etc/postgresql/$my_version/main/postgresql.conf
 sed -i "s/^log_timezone = .*/log_timezone = '$time_zone'/g" /etc/postgresql/$my_version/main/postgresql.conf
@@ -18,22 +18,19 @@ sed -i "s/^archive_command =.*/archive_command = 'test ! -f /wal_archive/%f && c
 sed -i 's/^wal_level =.*/wal_level = replica/g' /etc/postgresql/$my_version/main/postgresql.conf
 
 systemctl postgresql postgres
-su postgres
 
 read -s -p "Enter password for replication: " my_password
-psql -c "CREATE USER replication REPLICATION LOGIN CONNECTION LIMIT 10 ENCRYPTED PASSWORD '$my_password';"
+su postgres -c "psql -c \"CREATE USER replication REPLICATION LOGIN CONNECTION LIMIT 10 ENCRYPTED PASSWORD '$my_password';\""
 
 echo "========Base Backup========="
 
-pg_basebackup -Ureplication -h127.0.0.1 --progress -D /basebackup/ -p$my_password
+#pg_basebackup -Ureplication -h127.0.0.1 --progress -D /basebackup/ -p$my_password
 
-ssh-keygen -N ""
 
 echo "please edit crontab:  crontab -e"
 echo "add script "
 echo "* * * * * rsync -avz /wal_archive/ postgres@pg-slave:/wal_archive/"
 echo "* * * * * rsync -avz /basebackup/ postgres@pg-slave:/basebackup/"
 
-exit
 
 echo "==========Completed============"
